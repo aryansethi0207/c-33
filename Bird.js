043edc504f4c092f2f26db2fdb6492a4420cc9e7
@aryansethi0207 <aryansethi0207@gmail.com>
2021-02-18 19:51:20 +0530

    modified speed

--- Bird.js
+++ Bird.js
@@ -16,8 +16,7 @@ class Bird extends BaseClass {
       var position = [this.body.position.x, this.body.position.y];
       this.trajectory.push(position);
     }
-   
-
+  
     for(var i=0; i<this.trajectory.length; i++){
       image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
     }
